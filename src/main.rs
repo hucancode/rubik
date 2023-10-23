641a615661e81a61441164fce2e75634b5adf9ea
hucancode <hucancode@gmail.com>
2023-10-23 16:11:37 +0900

    triangle

---
+++ src/main.rs
@@ -0,0 +1,28 @@
+use hello_wgpu::run;
+use winit::event_loop::EventLoop;
+use winit::window::Window;
+fn main() {
+    let event_loop = EventLoop::new();
+    let window = Window::new(&event_loop).unwrap();
+    #[cfg(not(target_arch = "wasm32"))]
+    {
+        env_logger::init();
+        pollster::block_on(run(event_loop, window));
+    }
+    #[cfg(target_arch = "wasm32")]
+    {
+        std::panic::set_hook(Box::new(console_error_panic_hook::hook));
+        console_log::init().expect("could not initialize logger");
+        use winit::platform::web::WindowExtWebSys;
+        // On wasm, append the canvas to the document body
+        web_sys::window()
+            .and_then(|win| win.document())
+            .and_then(|doc| doc.body())
+            .and_then(|body| {
+                body.append_child(&web_sys::Element::from(window.canvas()))
+                    .ok()
+            })
+            .expect("couldn't append canvas to document body");
+        wasm_bindgen_futures::spawn_local(run(event_loop, window));
+    }
+}
