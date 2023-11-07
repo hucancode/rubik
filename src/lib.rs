6c6483ded5d171f7ce8a7fd4b6d2aa94ad5947ac
hucancode <hucancode@gmail.com>
2023-11-07 15:43:32 +0900

    nicer rotation

--- src/lib.rs
+++ src/lib.rs
@@ -4,6 +4,7 @@ mod world;
 
 use geometry::Geometry;
 use shader::Shader;
+use std::f32::consts::PI;
 use std::ops::Add;
 use std::sync::Arc;
 use std::time::{Duration, Instant};
@@ -16,7 +17,6 @@ use world::Node;
 
 const CUBE_SIZE: f32 = 2.0;
 const CUBE_MARGIN: f32 = 0.15;
-const ROTATE_CYCLE: u128 = 5000;
 const MAX_FPS: u64 = 60;
 const TARGET_FRAME_TIME: Duration = Duration::from_millis(1000 / MAX_FPS);
 
@@ -29,16 +29,20 @@ pub async fn run(event_loop: EventLoop<()>, window: Window) {
         include_str!("shader/shader.wgsl"),
     ));
     let d = CUBE_SIZE + CUBE_MARGIN;
-    for x in -1..=1 {
+    let mut row_transforms = Vec::new();
+    for z in -1..=1 {
+        let mut row = Node::new_empty();
         for y in -1..=1 {
-            for z in -1..=1 {
+            for x in -1..=1 {
                 let rubik = Node::new(rubik_mesh.clone(), shader.clone());
                 let transform = rubik.transform.clone();
                 let mut transform = transform.lock().unwrap();
                 transform.translation = glam::Vec3::new(d * x as f32, d * y as f32, d * z as f32);
-                renderer.root.add_child(Arc::new(rubik));
+                row.add_child(Arc::new(rubik));
             }
         }
+        row_transforms.push(row.transform.clone());
+        renderer.root.add_child(Arc::new(row));
     }
     let rubik = Node::new(cube_mesh, shader.clone());
     let transform = rubik.transform.clone();
@@ -46,11 +50,18 @@ pub async fn run(event_loop: EventLoop<()>, window: Window) {
     let app_start_time = Instant::now();
     let update = move || {
         let time = app_start_time.elapsed().as_millis();
-        let rotation = 2.0 * 3.14 * 0.001 * (time % ROTATE_CYCLE) as f32;
+        let rx = PI*2.0*((time as f64)*0.00042).sin() as f32;
+        let ry = PI*2.0*((time as f64)*0.00011).sin() as f32;
+        let rz = PI*2.0*((time as f64)*0.00027).sin() as f32;
         let mut transform = transform.lock().unwrap();
-        transform.rotation = glam::Quat::from_rotation_z(rotation);
+        transform.rotation = glam::Quat::from_euler(glam::EulerRot::XYZ,rx,ry,rz);
         let z = 4.0 + (time as f64 / 1000.0).sin() as f32;
         transform.translation = glam::Vec3::new(0.0, 0.0, z);
+        for (i,transform) in row_transforms.iter().enumerate() {
+            let mut transform = transform.lock().unwrap();
+            let alpha = ((time as f64)*0.001 + (i as f64)*0.2).sin();
+            transform.rotation = glam::Quat::from_rotation_z(alpha as f32);
+        }
     };
     let mut last_update_time = Instant::now();
     event_loop.run(move |event, _, control_flow| {
