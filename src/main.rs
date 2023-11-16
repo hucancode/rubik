49565784159b3da9dfbd59047b4b3b531b7613c8
hucancode <hucancode@gmail.com>
2023-11-16 12:12:01 +0900

    window title

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ use winit::window::Window;
 fn main() {
     let event_loop = EventLoop::new();
     let window = Window::new(&event_loop).unwrap();
+    window.set_title("Rubik");
     env_logger::init();
     pollster::block_on(run(event_loop, window));
 }
