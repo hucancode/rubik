168b9994f84b1b9bcd93d7b4e74fcab24c11dfa5
hucancode <hucancode@gmail.com>
2025-07-25 23:44:21 +0900

    fix wasm build

--- build-run-web.sh
+++ build-run-web.sh
@@ -9,6 +9,6 @@ WASM_OUTPUT=./target/$TARGET/$CONFIG/rubik.wasm
 WEB_OUTPUT=web
 
 echo "Building for $CONFIG"
-cargo build --target $TARGET $BUILD_CONFIG || { echo "cargo build failed"; exit 1; }
+RUSTFLAGS='--cfg getrandom_backend="wasm_js"' cargo build --target $TARGET $BUILD_CONFIG || { echo "cargo build failed"; exit 1; }
 wasm-bindgen --out-dir $WEB_OUTPUT --target web $WASM_OUTPUT --no-typescript || { echo "wasm-bindgen failed"; exit 1; }
 python3 -m http.server --directory $WEB_OUTPUT
